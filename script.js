0cd312fb35d106711ba43cbda77b07429bc9f45b
Muhammad Irfan <muhammadirfan.devx@gmail.com>
2026-03-09 13:59:49 +0500

    TLW-6: Fix audio base64 URL - added data: prefix

---
+++ script.js
@@ -0,0 +1,4 @@
+function playNotificationSound() {
+    const audio = new Audio('data:audio/wav;base64,UklGRnoGAABXQVZFZm10IBAAAAABAAEAQB8AAEAfAAABAAgAZGF0YQoGAACBhYqFbF1fdJivrJBhNjVgodDbq2EcBj+a2/LDciUFLIHO8tiJNwgZaLvt559NEAxQp+PwtmMcBjiR1/LMeSwFJHfH8N2QQAoUXrTp66hVFApGn+DyvmwhBTGH0fPTgjMGHm7A7+OZURE');
+    audio.play().catch(e => console.log('Audio play failed'));
+}
